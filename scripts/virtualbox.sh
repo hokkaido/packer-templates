5f2f25275361f3f175dc0a1511c287fd8b218a1f
hokkaido <hello@kimchi.io>
2014-04-09 18:06:04 +0200

    virtualbox guest additions: mount and installation

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,1 +1,9 @@
-apt-get -y -q install linux-headers-$(uname -r) build-essential dkms
+    apt-get install -y linux-headers-$(uname -r) build-essential dkms
+
+
+    VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+    mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+
+    sh /mnt/VBoxLinuxAdditions.run
+    umount /mnt
+    rm /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso
